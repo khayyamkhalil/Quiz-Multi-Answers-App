e26768eb060f580d53f0d3bf34b30813a5ba3804
Khayyam <khayyam@friendlitech.com>
2021-11-07 10:38:36 -0800

    Initial Commit

---
+++ Quiz-Multi-Answers-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz-Multi-Answers-App
+//
+//  Created by Khayyam Khalil on 11/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
